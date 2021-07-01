ee68f4c0554bbe634dc0d8ed039fac4c7b4b8ea1
Angela Gilhotra <angelagilhotra@gmail.com>
2021-07-01 21:41:03 +0530

    new: add link to collection on opensea in card footer

--- src/components/sections/Footer.vue
+++ src/components/sections/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="pt-5 sm:pt-10 sm:mt-3 card-footer sm:absolute bottom-0 sm:bottom-4"
+    class="pt-5 card-footer bottom-0 sm:absolute sm:bottom-4 sm:pt-10 sm:mt-3"
   >
-    <div class="font-sans text-gray-400 text-sm">
+    <div class="font-sans text-gray-400 text-sm sm:text-center">
       <span>
         Design & code by
       </span>
@@ -14,6 +14,14 @@
       </span>
       <a href="https://www.instagram.com/malayvasa/" target="_new">
         @malayvasa
+      </a>
+      <br />
+      <a
+        href="https://opensea.io/collection/kernel-fellowship-gratitude"
+        target="_new"
+        class="text-xs"
+      >
+        View collection on OpenSea
       </a>
     </div>
   </div>
