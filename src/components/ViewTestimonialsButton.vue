84198bff1de4d9a13cdd0f640012098aae8a3ea6
Angela Gilhotra <angelagilhotra@gmail.com>
2021-07-26 10:24:15 +0530

    fix

--- src/components/ViewTestimonialsButton.vue
+++ src/components/ViewTestimonialsButton.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="font-sans">
-    <div class="sm:p-0 pt-2 float-right ">
+  <div
+    class="font-sans cursor-pointer p-2"
+    @click="$store.commit('togglePanel', true)"
+  >
+    <button class="sm:p-0 pt-2 float-right">
       <button
         class="text-sm sm:text-base bg-gray-50 font-fancy border-opacity-50 border-dashed border-b-2 border-gray-900"
-        @click="$store.commit('togglePanel', true)"
       >
         View Testimonials
       </button>
-    </div>
+    </button>
   </div>
 </template>
 <script>
